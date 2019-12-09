8559893fef769b8c8c0f545c807792e42702e772
Melanie Villela <melanienvillela@gmail.com>
2019-12-09 13:20:05 -0600

    Update
    Added config files.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
-};
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/contact": { page: "/contact" },
+      "/index": { page: "/index" },
+      "/personal": { page: "/personal" },
+      "/professional": { page: "/professional" },
+    }
+  },
+  assetPrefix: !debug ? '/' : '',
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    return config
+  }
+}
