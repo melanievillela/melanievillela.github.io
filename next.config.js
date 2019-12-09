8319a92171890b124968a5e1c5c2ba928a42433f
Melanie Villela <melanienvillela@gmail.com>
2019-12-09 12:53:50 -0600

    Update
    Updated resume and trying to get proper page routing setup.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/melanievillela.github.io' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
 };
