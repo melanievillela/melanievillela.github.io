7e7c4bdf088dfaf63edc880d731a718ac28b318a
Melanie <melanienvillela@gmail.com>
2020-07-28 13:14:42 -0500

    Updated animations to use JS only

--- js/script.js
+++ js/script.js
@@ -1,17 +1,49 @@
 //Bounce in effects
-const icons = document.getElementsByClassName("icons");
+const icons = document.querySelectorAll(".icons");
 
-function timedText() {
-    setTimeout(function(){ icons[0].classList.add("bounce") }, 500);
-    setTimeout(function(){ icons[1].classList.add("bounce") }, 1000);
-    setTimeout(function(){ icons[2].classList.add("bounce") }, 1500);
-    setTimeout(function(){ icons[0].classList.add("bounce-final") }, 1500);
-    setTimeout(function(){ icons[1].classList.add("bounce-final") }, 2000);
-    setTimeout(function(){ icons[2].classList.add("bounce-final") }, 2500);
+function timedIcons() {
+    setTimeout(function(){ icons[0].classList.add("animate__animated", "animate__bounceInDown") }, 500);
+    setTimeout(function(){ icons[1].classList.add("animate__animated", "animate__bounceInDown") }, 1000);
+    setTimeout(function(){ icons[2].classList.add("animate__animated", "animate__bounceInDown") }, 1500);
 }
 
-//Hover project display effects
-const projects = document.getElementsByClassName("projects");
+//Loop through skills
+const skills = ["JAVASCRIPT","PYTHON","ILLUSTRATOR","FLASK","NODE","PHOTOSHOP","REACT","AMAZON AWS","EXPRESS","WORDPRESS","JQUERY"];
+const skillsContainer = document.querySelector(".skills");
+
+function timedText() {
+    let i = 0;
+
+    setInterval(function(){
+            skillsContainer.textContent = skills[i];
+            i++;
+            if(i>=skills.length) {
+                i = 0;
+                timedText();
+            }
+    }, 1000);    
+}
+
+//Project display effects
+const projects = document.querySelectorAll(".projects");
+
+//Check visibility
+function isScrolledIntoView(el) {
+    let rect = el.getBoundingClientRect();
+    let elTop = rect.top;
+    let elBottom = rect.bottom;
+    let isVisible = elTop < window.innerHeight && elBottom >= 0;
+    return isVisible;
+}
+
+//Check visibility
+function checkVisibility() {
+    projects.forEach(project => {
+        if(isScrolledIntoView(project)) {
+            project.classList.add("animate__animated", "animate__slideInRight");
+        }
+    });
+}
 
 function toggleInfo() {
     for (let i = 0; i < projects.length; i++) {
@@ -24,10 +56,9 @@ function toggleInfo() {
     } 
 }
 
+timedIcons();
 timedText();
 toggleInfo();
+window.addEventListener('scroll', checkVisibility);
 
 console.log("Icons made by Freepik at https://www.flaticon.com/authors/freepik")
-
-//Modernizr
-!function(e,n,t){function r(e,n){return typeof e===n}function o(){var e,n,t,o,s,i,a;for(var l in S)if(S.hasOwnProperty(l)){if(e=[],n=S[l],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(t=0;t<n.options.aliases.length;t++)e.push(n.options.aliases[t].toLowerCase());for(o=r(n.fn,"function")?n.fn():n.fn,s=0;s<e.length;s++)i=e[s],a=i.split("."),1===a.length?Modernizr[a[0]]=o:(!Modernizr[a[0]]||Modernizr[a[0]]instanceof Boolean||(Modernizr[a[0]]=new Boolean(Modernizr[a[0]])),Modernizr[a[0]][a[1]]=o),C.push((o?"":"no-")+a.join("-"))}}function s(e){var n=_.className,t=Modernizr._config.classPrefix||"";if(x&&(n=n.baseVal),Modernizr._config.enableJSClass){var r=new RegExp("(^|\\s)"+t+"no-js(\\s|$)");n=n.replace(r,"$1"+t+"js$2")}Modernizr._config.enableClasses&&(n+=" "+t+e.join(" "+t),x?_.className.baseVal=n:_.className=n)}function i(e,n){return!!~(""+e).indexOf(n)}function a(){return"function"!=typeof n.createElement?n.createElement(arguments[0]):x?n.createElementNS.call(n,"http://www.w3.org/2000/svg",arguments[0]):n.createElement.apply(n,arguments)}function l(e){return e.replace(/([a-z])-([a-z])/g,function(e,n,t){return n+t.toUpperCase()}).replace(/^-/,"")}function u(e,n){return function(){return e.apply(n,arguments)}}function f(e,n,t){var o;for(var s in e)if(e[s]in n)return t===!1?e[s]:(o=n[e[s]],r(o,"function")?u(o,t||n):o);return!1}function c(e){return e.replace(/([A-Z])/g,function(e,n){return"-"+n.toLowerCase()}).replace(/^ms-/,"-ms-")}function d(n,t,r){var o;if("getComputedStyle"in e){o=getComputedStyle.call(e,n,t);var s=e.console;if(null!==o)r&&(o=o.getPropertyValue(r));else if(s){var i=s.error?"error":"log";s[i].call(s,"getComputedStyle returning null, its possible modernizr test results are inaccurate")}}else o=!t&&n.currentStyle&&n.currentStyle[r];return o}function p(){var e=n.body;return e||(e=a(x?"svg":"body"),e.fake=!0),e}function m(e,t,r,o){var s,i,l,u,f="modernizr",c=a("div"),d=p();if(parseInt(r,10))for(;r--;)l=a("div"),l.id=o?o[r]:f+(r+1),c.appendChild(l);return s=a("style"),s.type="text/css",s.id="s"+f,(d.fake?d:c).appendChild(s),d.appendChild(c),s.styleSheet?s.styleSheet.cssText=e:s.appendChild(n.createTextNode(e)),c.id=f,d.fake&&(d.style.background="",d.style.overflow="hidden",u=_.style.overflow,_.style.overflow="hidden",_.appendChild(d)),i=t(c,e),d.fake?(d.parentNode.removeChild(d),_.style.overflow=u,_.offsetHeight):c.parentNode.removeChild(c),!!i}function y(n,r){var o=n.length;if("CSS"in e&&"supports"in e.CSS){for(;o--;)if(e.CSS.supports(c(n[o]),r))return!0;return!1}if("CSSSupportsRule"in e){for(var s=[];o--;)s.push("("+c(n[o])+":"+r+")");return s=s.join(" or "),m("@supports ("+s+") { #modernizr { position: absolute; } }",function(e){return"absolute"==d(e,null,"position")})}return t}function v(e,n,o,s){function u(){c&&(delete N.style,delete N.modElem)}if(s=r(s,"undefined")?!1:s,!r(o,"undefined")){var f=y(e,o);if(!r(f,"undefined"))return f}for(var c,d,p,m,v,g=["modernizr","tspan","samp"];!N.style&&g.length;)c=!0,N.modElem=a(g.shift()),N.style=N.modElem.style;for(p=e.length,d=0;p>d;d++)if(m=e[d],v=N.style[m],i(m,"-")&&(m=l(m)),N.style[m]!==t){if(s||r(o,"undefined"))return u(),"pfx"==n?m:!0;try{N.style[m]=o}catch(h){}if(N.style[m]!=v)return u(),"pfx"==n?m:!0}return u(),!1}function g(e,n,t,o,s){var i=e.charAt(0).toUpperCase()+e.slice(1),a=(e+" "+P.join(i+" ")+i).split(" ");return r(n,"string")||r(n,"undefined")?v(a,n,o,s):(a=(e+" "+z.join(i+" ")+i).split(" "),f(a,n,t))}function h(e,n,r){return g(e,t,t,n,r)}var C=[],S=[],w={_version:"3.6.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var t=this;setTimeout(function(){n(t[e])},0)},addTest:function(e,n,t){S.push({name:e,fn:n,options:t})},addAsyncTest:function(e){S.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=w,Modernizr=new Modernizr;var _=n.documentElement,x="svg"===_.nodeName.toLowerCase(),b="Moz O ms Webkit",P=w._config.usePrefixes?b.split(" "):[];w._cssomPrefixes=P;var z=w._config.usePrefixes?b.toLowerCase().split(" "):[];w._domPrefixes=z;var E={elem:a("modernizr")};Modernizr._q.push(function(){delete E.elem});var N={style:E.elem.style};Modernizr._q.unshift(function(){delete N.style}),w.testAllProps=g,w.testAllProps=h,Modernizr.addTest("cssanimations",h("animationName","a",!0)),o(),s(C),delete w.addTest,delete w.addAsyncTest;for(var T=0;T<Modernizr._q.length;T++)Modernizr._q[T]();e.Modernizr=Modernizr}(window,document);
